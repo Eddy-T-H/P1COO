44526cca702db9cd074264c81e307ea845e6c42c
Antoine.b <antoine.blondin01@gmail.com>
2015-09-29 16:13:16 +0200

    Parse plus adapté pour la commande move

--- src/dungeon/MoveCommand.java
+++ src/dungeon/MoveCommand.java
@@ -4,7 +4,14 @@ public class MoveCommand implements InterfaceCommand{
 
 	@Override
 	public void action(String command, AbstractDungeon dungeon) {
-		dungeon.currentRoom.exit(dungeon, command);
+		switch(command.toLowerCase()){
+		case "west":
+		case "north":
+		case "south":
+		case "east": 	dungeon.currentRoom.exit(dungeon, command);
+						break;
+		default: 		System.out.println("/!\\ Wrong direction: '" + command + "' isn't recognized /!\\");
+		}
 	}
 
 }
